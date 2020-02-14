b21c25d6a5e25dae6035629bbd03697830a851e9
Ken <ken@Kens-MacBook-Pro.local>
2020-02-14 11:10:07 -0800

    add styles.js to Navbar, update styling

--- imports/ui/components/NavBar/NavBar.js
+++ imports/ui/components/NavBar/NavBar.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
+import styles from './styles'
+import {withStyles, AppBar, Toolbar} from '@material-ui/core'
 
 const NavBar = ({ classes }) => {
     return (
-        <nav>
+        <nav className={classes.root}>
+        <AppBar className={classes.appbar} position="static">
+        <Toolbar>
         <NavLink to="/feed" activeClassName="selected">
             <img src={'/images/logo/logo.svg'} height="100px" width="100px" alt="logo"/>
             Feed</NavLink>
@@ -11,9 +15,20 @@ const NavBar = ({ classes }) => {
         <NavLink to="/account" activeClassName="selected">Account</NavLink>
         <NavLink to="/signup" activeClassName="selected">SignUp</NavLink>
         <NavLink to="/single" activeClassName="selected">Single</NavLink>
+        </Toolbar>
+      </AppBar>
+
+          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button> */}
+
     </nav>
       ) 
 }
 
 
-export default withRouter(NavBar);
+export default withRouter(withStyles(styles)(NavBar));
