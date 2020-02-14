eeb039e87031e9ee91f132527ab66169cede9f3b
Brooke <brooke.xiang@gmail.com>
2020-02-14 14:56:17 -0800

    add single page content

---
+++ imports/ui/pages/Single/Single.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "@material-ui/core";
+const Single = ({ users, events }) => {
+  console.log("hello", users);
+  console.log("goodbye", events);
+
+  return (
+    <Container>
+      <div className="banner">poop</div>
+    </Container>
+  );
+};
+export default Single;
