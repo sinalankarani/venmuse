62ef3caf21fef5a559b9a24df041e0e7b80bf287
Kieran Devlin <kjrdevlin@gmail.com>
2020-02-14 11:01:01 -0800

    Add mongo methods and pub

--- imports/api/index.js
+++ imports/api/index.js
@@ -1,2 +1,22 @@
-import { Mongo } from "meteor/mongo";
-export const Events = new Mongo.Collection("events");
+import { Mongo } from 'meteor/mongo';
+export const Events = new Mongo.Collection('events');
+
+if (Meteor.isServer) {
+  Meteor.publish('events', function todosPublication() {
+    return Events.find();
+  });
+}
+
+Meteor.methods({
+  // event param will be object containing all necessary data
+  // then add to events collections
+  'events.addNewEvent'(event) {
+    if (user.profile.userType !== 'venue') {
+      throw new Meteor.Error(
+        'events.addNewEvent.not-authorized',
+        'You are unauthorized to add new events.'
+      );
+    }
+    Events.insert(event);
+  }
+});
