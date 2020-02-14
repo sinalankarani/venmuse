15dd0154c44f2d5f3b8537d9575b04fd13c366cf
Ken <ken@Kens-MacBook-Pro.local>
2020-02-14 10:03:19 -0800

    manually imported routes content, created public folder for images

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from "./routes/";
+
 
 const App = () => {
-  return <h1>VenMuse</h1>;
+  return (
+  <div className="container">
+  <Router>
+    <Routes />
+  </Router>
+</div>
+  )
+
 };
 
 export default App;
---
+++ imports/ui/components/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink, withRouter } from 'react-router-dom';
+
+const NavBar = ({ classes }) => {
+    return (
+        <nav>
+        <NavLink to="/feed" activeClassName="selected">
+            <img src={'/images/logo/logo.svg'} alt="logo"/>
+            Feed</NavLink>
+        <NavLink to="/archive" activeClassName="selected">Archive</NavLink>
+        <NavLink to="/account" activeClassName="selected">Account</NavLink>
+        <NavLink to="/signup" activeClassName="selected">SignUp</NavLink>
+        <NavLink to="/single" activeClassName="selected">Single</NavLink>
+    </nav>
+      ) 
+}
+
+
+export default withRouter(NavBar);
