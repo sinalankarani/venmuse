9ff4e064be77c43637111f7f11ad43ac21417a99
Ken <ken@Kens-MacBook-Pro.local>
2020-02-14 13:41:46 -0800

    update VenueCard content

--- imports/ui/components/VenueCard/VenueCard.js
+++ imports/ui/components/VenueCard/VenueCard.js
@@ -4,11 +4,26 @@ import { Card, Typography } from "@material-ui/core";
 const VenueCard = ({venues}) => {
 return (
             venues.map(venue => {
-                console.log(venue)
                 if(venue.profile.userType==="venue"){
+                    console.log(venue)
                     return (
                         <Card key={venue._id}>
+                            <h1>VENUE CARD</h1> {/*REMOVE LATER*/}
+                            {/* PROFILE IMAGE */}
+                            <div>
+                                <img src={venue.profile.profileImage} alt="profile-image" height="300px" width="600px"/>
+                            </div>
+                            {/* USERNAME */}
                             <Typography>{venue.username}</Typography>
+                            {/* EMAIL */}
+                            {venue.emails.map(email => {
+                                return (
+                                    <Typography key={venue._id}>{email.address}</Typography>
+                                )
+                            })}
+                            {/* LOCATION */}
+                            <Typography>{venue.profile.location}</Typography>
+
                         </Card>
                         )
                     }
