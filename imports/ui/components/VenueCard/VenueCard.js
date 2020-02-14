6e3135101979820b6b681cdc62f2886d778a4b02
Ken <ken@Kens-MacBook-Pro.local>
2020-02-14 10:43:40 -0800

    create VenueCard directory (VenueCard.js, index.js, styles.js)

---
+++ imports/ui/components/VenueCard/VenueCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Card } from "@material-ui/core";
+
+const VenueCard = () => {
+  return (
+    <Card>
+      <h1>Events Card</h1>
+    </Card>
+  );
+};
+
+export default VenueCard;
