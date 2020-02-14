b3b1576ca916df4f824c900ec996d9c167c7e138
Brooke <brooke.xiang@gmail.com>
2020-02-14 13:08:24 -0800

    add theme directory

---
+++ imports/ui/AccountsForm/styles.js
@@ -0,0 +1,10 @@
+import { createStyles } from "@material-ui/core";
+
+const styles = (theme = createStyles({
+  form: {
+    display: "flex",
+    flexDirection: "column"
+  }
+}));
+
+export default styles;
---
+++ imports/ui/theme/index.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#8f24d1"
+    },
+    secondary: {
+      main: "#fff"
+    }
+  }
+});
