9c0555474fd5f743265ef752e754e03e467f684b
Brooke <brooke.xiang@gmail.com>
2020-02-16 12:56:18 -0800

    add account page

--- imports/ui/pages/Account/Account.js
+++ imports/ui/pages/Account/Account.js
@@ -1,1 +1,18 @@
+import React from "react";
 import AccountsForm from "../../AccountsForm";
+import NavBar from "../../components/NavBar";
+
+class Login extends React.Component {
+  render() {
+    console.log("on accounts page");
+    return (
+      <React.Fragment>
+        <NavBar />
+        <AccountsForm />
+        <p>poop</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Login;
