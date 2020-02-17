103a0862c54890f98c5a88de00ca769500af1a52
Ken <ken@Kens-MacBook-Pro.local>
2020-02-17 14:38:09 -0800

    merge

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,11 +2,8 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./routes/";
 import EventsContainer from "../ui/components/EventsContainer";
-<<<<<<< HEAD
 import VenueContainer from "../ui/containers/VenueContainer";
-=======
 import ArtistContainer from "../ui/containers/ArtistContainer";
->>>>>>> master
 
 import { Events } from "../api";
 import { withTracker } from "meteor/react-meteor-data";
@@ -19,10 +16,6 @@ class App extends React.Component {
           <Routes />
         </Router>
         <h1>VenMuse</h1>
-<<<<<<< HEAD
-        <VenueContainer venues={this.props.users}/>
-=======
->>>>>>> master
         <EventsContainer events={this.props.events} />
       </React.Fragment>
     );
