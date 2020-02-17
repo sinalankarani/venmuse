710ad8f3f72ed2bca6830d8c9a3ae9eefbfaa057
Ken <ken@Kens-MacBook-Pro.local>
2020-02-17 13:49:45 -0800

    link ArtistContainer to routes

---
+++ imports/ui/pages/Archive/Archive.js
@@ -0,0 +1,8 @@
+import ArtistContainer from "../../containers/ArtistContainer";
+import React from "react";
+
+const Archive = () => {
+  return <ArtistContainer />;
+};
+
+export default Archive;
---
+++ imports/ui/pages/Feed/Feed.js
@@ -0,0 +1,8 @@
+// import ArtistContainer from "../../containers/ArtistContainer";
+// import React from "react";
+
+// const Feed = () => {
+//   return <p>hello</p>;
+// };
+
+// export default Feed;
