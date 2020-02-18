96d88fbc4496204a8098c533c2827f00c463a057
Kieran Devlin <kjrdevlin@gmail.com>
2020-02-18 10:05:46 -0800

    Merge Account page

--- imports/ui/pages/Account/Account.js
+++ imports/ui/pages/Account/Account.js
@@ -1,31 +1,15 @@
-import React from "react";
-<<<<<<< HEAD
-import AccountsForm from "../../AccountsForm";
-import NavBar from "../../components/NavBar";
+import React from 'react';
+import AccountsForm from '../../AccountsForm';
+import NavBar from '../../components/NavBar';
 
-class Login extends React.Component {
+class Account extends React.Component {
   render() {
-    console.log("on accounts page");
+    console.log('on accounts page');
     return (
       <React.Fragment>
-        <NavBar />
         <AccountsForm />
-        <p>poop</p>
       </React.Fragment>
     );
   }
 }
-
-export default Login;
-=======
-
-const Account = () => {
-  return (
-    <div>
-      <h1>account page</h1>
-    </div>
-  );
-};
-
 export default Account;
->>>>>>> master
