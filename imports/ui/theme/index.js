8f1a790c0e4996646a13af23825e40e3a494f923
Brooke <brooke.xiang@gmail.com>
2020-02-18 15:23:44 -0800

    update accounts form

--- imports/ui/theme/index.js
+++ imports/ui/theme/index.js
@@ -3,7 +3,7 @@ import { createMuiTheme } from "@material-ui/core/styles";
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: "#8f24d1"
+      main: "#FF9633"
     },
     secondary: {
       main: "#fff"
