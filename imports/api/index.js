60779750108fea5f6b955322f9bf260e9b838616
Kieran Devlin <kjrdevlin@gmail.com>
2020-02-18 15:29:06 -0800

    Finish new event mongo method and form

--- imports/api/index.js
+++ imports/api/index.js
@@ -3,40 +3,36 @@ import { Meteor } from 'meteor/meteor';
 export const Events = new Mongo.Collection('events');
 
 if (Meteor.isServer) {
-  Meteor.publish('events', function todosPublication() {
+  Meteor.publish('events', function eventsPublication() {
     return Events.find();
   });
-  /////////////////////////////////// NEED TO REMOVE LATER
-  Meteor.publish('users', function todosPublication() {
+  //////////////////KIERAN WILL REMOVE LATER - NOONE ELSE!
+  Meteor.publish('users', function usersPublication() {
     return Meteor.users.find();
   });
   ////////////////////////////////////////////////////////
 }
 
 Meteor.methods({
-  // event param will be object containing all necessary data
-  // then add to events collections
   'events.addNewEvent'(event) {
-    // if (user.profile.userType !== 'venue') {
-    //   throw new Meteor.Error(
-    //     'events.addNewEvent.not-authorized',
-    //     'You are unauthorized to add new events.'
-    //   );
-    // }
-    // Events.insert({...event, owner:this.userId}); ------ destruct event object
-    // or try spreading and adding any other needed info
-    Events.insert(event);
+    if (Meteor.user().profile.userType !== 'venue') {
+      throw new Meteor.Error(
+        'events.addNewEvent.not-authorized',
+        'You are unauthorized to add new events.'
+      );
+    }
+    Events.insert({ ...event, owner: this.userId, filled: false });
   },
 
   'users.updateProfile'(newProfileData) {
-    // if(newProfileData!this.userId){
-    //   throw new Meteor.Error(
-    //     'profile.updateProfile.not-authorized',
-    //     'You are not allowed to update profile for other users'
-    //   );
-    // }
-    // need to test if correct user and test if newProfileData
-    // overwrites current profile data
-    Meteor.users.update(users._id, { profile: { ...newProfileData } });
+    if (!this.userId) {
+      throw new Meteor.Error(
+        'profile.updateProfile.not-authorized',
+        'You are not allowed to update profile for other users'
+      );
+    }
+    Meteor.users.update(Meteor.userId(), {
+      $set: { profile: { ...Meteor.user().profile, ...newProfileData } }
+    });
   }
 });
--- imports/ui/components/SubmitEvent/SubmitEvent.js
+++ imports/ui/components/SubmitEvent/SubmitEvent.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Events } from '../../../api/';
 import { Form, Field } from 'react-final-form';
+import { withTracker } from 'meteor/react-meteor-data';
+import { withRouter } from 'react-router-dom';
 import validate from './helpers/validation';
 import { Meteor } from 'meteor/meteor';
 import styles from './styles';
@@ -8,111 +11,204 @@ import {
   Card,
   TextField,
   Typography,
-  withStyles
+  withStyles,
+  MenuItem,
+  Checkbox,
+  ListItemText,
+  Select,
+  FormControl,
+  InputLabel,
+  Input
 } from '@material-ui/core';
 import moment from 'moment';
 
-const SubmitEvent = ({ classes }) => {
-  const onSubmit = values => {
+class SubmitEvent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onSubmit = values => {
     const newEvent = {
       date: values.date,
       created: moment()
         .startOf()
         .fromNow(),
       title: values.title,
-      /////THIS NEEDS TO BE VENUE TITLE
-      location: 'Da club',
-      /////////////////////////////////
-      description: ''
+      location:
+        this.props.user && this.props.user.profile.location
+          ? this.props.user.profile.location
+          : values.location,
+      description: values.description,
+      tags: values.tags
     };
     Meteor.call('events.addNewEvent', newEvent);
+    this.props.history.push('/feed');
   };
 
-  return (
-    <Card className={classes.formContainer}>
-      <Typography variant="h4">Submit New Event</Typography>
-      <Form
-        onSubmit={onSubmit}
-        validate={validate.bind(this)}
-        render={({ handleSubmit, form, pristine }) => {
-          return (
-            <form onSubmit={handleSubmit} className={classes.form}>
-              {/* EVENT TITLE */}
-              <Field
-                name="title"
-                type="text"
-                render={({ input, meta }) => (
-                  <React.Fragment>
-                    <TextField
-                      label="Title"
-                      className={classes.fields}
-                      {...input}
-                    />
-                    {meta.error && meta.touched && (
-                      <span className={classes.error}>{meta.error}</span>
-                    )}
-                  </React.Fragment>
-                )}
-              />
+  render() {
+    const { classes } = this.props;
+    const tags = [
+      'Blues',
+      'Classical',
+      'Comedy',
+      'Country',
+      'Dance Performance',
+      'Electronic',
+      'Folk',
+      'Hip hop',
+      'Indie',
+      'Jazz',
+      'Musical Theatre',
+      'Metal',
+      'Pop',
+      'Punk',
+      'R&B',
+      'Rap',
+      'Reggae',
+      'Rock',
+      'Top 40'
+    ];
+    return (
+      <Card className={classes.formContainer}>
+        <Typography variant="h4">Submit New Event</Typography>
+        <Form
+          onSubmit={this.onSubmit}
+          validate={validate}
+          initialValues={{ tags: [] }}
+          render={({ handleSubmit, form, pristine }) => {
+            return (
+              <form onSubmit={handleSubmit} className={classes.form}>
+                {/* EVENT TITLE */}
+                <Field
+                  name="title"
+                  type="text"
+                  render={({ input, meta }) => (
+                    <>
+                      <TextField
+                        label="Title"
+                        className={classes.fields}
+                        {...input}
+                      />
+                      {meta.error && meta.touched && (
+                        <span className={classes.error}>{meta.error}</span>
+                      )}
+                    </>
+                  )}
+                />
+                {/* EVENT LOCATION */}
 
-              {/* EVENT DESCRIPTION */}
-              <Field
-                name="description"
-                type="text"
-                render={({ input, meta }) => (
-                  <React.Fragment>
-                    <TextField
-                      label="Description"
-                      className={classes.fields}
-                      {...input}
-                    />
-                    {meta.error && meta.touched && (
-                      <span className={classes.error}>{meta.error}</span>
+                {this.props.user && !this.props.user.profile.location && (
+                  <Field
+                    name="location"
+                    type="text"
+                    render={({ input, meta }) => (
+                      <>
+                        <TextField
+                          label="Location"
+                          className={classes.fields}
+                          {...input}
+                        />
+                        {meta.error && meta.touched && (
+                          <span className={classes.error}>{meta.error}</span>
+                        )}
+                      </>
                     )}
-                  </React.Fragment>
+                  />
                 )}
-              />
 
-              {/* EVENT DATE */}
-              <Field
-                name="date"
-                render={({ input, meta }) => (
-                  <React.Fragment>
-                    <TextField
-                      className={classes.fields}
-                      id="date"
-                      label="Date"
-                      type="date"
-                      {...input}
-                      InputLabelProps={{
-                        shrink: true
-                      }}
-                    />
-                    {meta.error && meta.touched && (
-                      <span className={classes.error}>{meta.error}</span>
-                    )}
-                  </React.Fragment>
-                )}
-              />
-              {/* SUBMIT NEW EVENT */}
-              <Button onClick={handleSubmit} variant="contained">
-                Submit
-              </Button>
-            </form>
-          );
-        }}
-      />
-    </Card>
-  );
-};
+                {/* EVENT DESCRIPTION */}
+                <Field
+                  name="description"
+                  type="text"
+                  render={({ input, meta }) => (
+                    <>
+                      <TextField
+                        label="Description"
+                        className={classes.fields}
+                        {...input}
+                      />
+                      {meta.error && meta.touched && (
+                        <span className={classes.error}>{meta.error}</span>
+                      )}
+                    </>
+                  )}
+                />
 
-export default withStyles(styles)(SubmitEvent);
-// - id
-// -due date (event date)
-// - date created
-// - title
-// -desc
-// -Venue.location (Google Maps iframe)
-// - Venue.id
-// - imageurl
-// -Artist.id []
+                {/* EVENT DATE */}
+                <Field
+                  name="date"
+                  render={({ input, meta }) => (
+                    <>
+                      <TextField
+                        className={classes.fields}
+                        id="date"
+                        label="Date"
+                        type="date"
+                        {...input}
+                        InputLabelProps={{
+                          shrink: true
+                        }}
+                      />
+                      {meta.error && meta.touched && (
+                        <span className={classes.error}>{meta.error}</span>
+                      )}
+                    </>
+                  )}
+                />
+
+                {/* EVENT TAGS */}
+                <Field
+                  name="tags"
+                  className={classes.fields}
+                  render={({ input, meta }) => (
+                    <>
+                      <FormControl className={classes.fields}>
+                        <InputLabel>Tags</InputLabel>
+                        <Select
+                          multiple
+                          input={<Input />}
+                          renderValue={selected => selected.join(', ')}
+                          {...input}
+                        >
+                          {tags.map(tag => (
+                            <MenuItem key={tag} value={tag}>
+                              <Checkbox
+                                checked={input.value.indexOf(tag) > -1}
+                              />
+                              <ListItemText primary={tag} />
+                            </MenuItem>
+                          ))}
+                        </Select>
+                      </FormControl>
+                      {meta.error && meta.touched && (
+                        <span className={classes.error}>{meta.error}</span>
+                      )}
+                    </>
+                  )}
+                />
+
+                {/* SUBMIT NEW EVENT */}
+                <Button
+                  type="submit"
+                  variant="contained"
+                  className={classes.submit}
+                >
+                  Submit
+                </Button>
+              </form>
+            );
+          }}
+        />
+      </Card>
+    );
+  }
+}
+export default withTracker(() => {
+  Meteor.subscribe('events');
+  Meteor.subscribe('users');
+  return {
+    events: Events.find({}).fetch(),
+    userId: Meteor.userId(),
+    user: Meteor.user()
+  };
+})(withStyles(styles)(withRouter(SubmitEvent)));
