4288d8f121a935bb05261110673f7eafce60f772
Sina <sina202@gmail.com>
2020-02-19 10:18:47 -0800

    display artists and venues card while checking userType and userId, add styling to user cards and containers

--- imports/ui/components/EventsCard/styles.js
+++ imports/ui/components/EventsCard/styles.js
@@ -2,14 +2,11 @@ const styles = () => {
   return {
     card: {
       maxWidth: 600,
-      height: 600
-    },
-    media: {
-      height: "50%"
+      height: 500
     },
     image: {
       width: "100%",
-      height: 350,
+      height: 300,
       borderRadius: 5,
       objectPosition: "center",
       objectFit: "cover"
