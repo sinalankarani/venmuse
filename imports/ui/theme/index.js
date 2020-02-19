7ab9b9e595099aafe1d79f94323d83caad018033
Kieran Devlin <kjrdevlin@gmail.com>
2020-02-19 11:15:23 -0800

    Add theme provider and fonts

--- imports/ui/theme/index.js
+++ imports/ui/theme/index.js
@@ -1,12 +1,20 @@
-import { createMuiTheme } from "@material-ui/core/styles";
+import { createMuiTheme } from '@material-ui/core/styles';
 
-const theme = createMuiTheme({
+export default createMuiTheme({
   palette: {
     primary: {
-      main: "#FF9633"
+      main: '#FF9633'
     },
     secondary: {
-      main: "#fff"
+      main: '#fff'
+    }
+  },
+  fonts: {
+    primary: {
+      main: 'Roboto, sans-serif'
+    },
+    secondary: {
+      main: 'Dhurjati, sans-serif'
     }
   }
 });
