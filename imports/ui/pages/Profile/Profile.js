f0b76566660a83cd5d4a789f634df4be93f88c13
Brooke <brooke.xiang@gmail.com>
2020-02-19 21:48:56 -0800

    finish single page -pastEvents

--- imports/ui/pages/Profile/Profile.js
+++ imports/ui/pages/Profile/Profile.js
@@ -4,17 +4,7 @@ import { withTracker } from "meteor/react-meteor-data";
 import { Events } from "../../../api";
 import styles from "./styles";
 import { withStyles } from "@material-ui/core";
-import {
-  Card,
-  Grid,
-  Modal,
-  Backdrop,
-  Fade,
-  Button,
-  Typography,
-  Box,
-  Link
-} from "@material-ui/core";
+import { Card, Grid, Modal, Backdrop, Fade, Button, Typography, Box } from "@material-ui/core";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import TwitterIcon from "@material-ui/icons/Twitter";
@@ -22,8 +12,9 @@ import { Meteor } from "meteor/meteor";
 import Gravatar from "react-gravatar";
 import Account from "../Account";
 import Loader from "../../components/Loader";
+import { Link } from "react-router-dom";
 
-const Profile = ({ user, users, userId, event, eventId, classes }) => {
+const Profile = ({ user, users, userId, event, pastEvents, eventId, classes }) => {
   const [openAccount, setOpenAccount] = React.useState(false);
   const [openEvent, setOpenEvent] = React.useState(false);
 
@@ -53,145 +44,172 @@ const Profile = ({ user, users, userId, event, eventId, classes }) => {
             {user ? <Gravatar className={classes.gravatar} email={user.emails[0].address} /> : null}
             <Box className={classes.titleLocation}>
               <Typography variant="h4"> {user.profile.title}</Typography>
-              <Typography variant="subtitle1">
+              <Typography variant="subtitle1" color="primary">
                 {user && user.profile && user.profile.location}
+              </Typography>
+              <Typography variant="body1">
+                {user && user.profile && user.profile.description}
               </Typography>
             </Box>
           </Box>
-
-          {user._id === userId ? (
-            <div>
-              <Button
-                type="button"
-                variant="contained"
-                size="large"
-                color="primary"
-                onClick={handleOpenAccount}
-              >
-                Update Profile{" "}
-              </Button>
-              <Modal
-                aria-labelledby="transition-modal-title"
-                aria-describedby="transition-modal-description"
-                className={classes.modal}
-                open={openAccount}
-                onClose={handleClose}
-                closeAfterTransition
-                BackdropComponent={Backdrop}
-                BackdropProps={{
-                  timeout: 500
-                }}
-              >
-                <Fade in={openAccount}>
-                  <div className={classes.paper}>
-                    <Account />
-                  </div>
-                </Fade>
-              </Modal>
-            </div>
-          ) : (
-            <div>
-              <Button
-                type="button"
-                variant="contained"
-                size="large"
-                color="primary"
-                onClick={handleOpenAccount}
-              >
-                Message Artist{" "}
-              </Button>
-              <Modal
-                aria-labelledby="transition-modal-title"
-                aria-describedby="transition-modal-description"
-                className={classes.modal}
-                open={openAccount}
-                onClose={handleClose}
-                closeAfterTransition
-                BackdropComponent={Backdrop}
-                BackdropProps={{
-                  timeout: 500
-                }}
-              >
-                <Fade in={openAccount}>
-                  <div className={classes.paper}></div>
-                </Fade>
-              </Modal>
-            </div>
-          )}
+          <Box className={classes.buttonsContainer}>
+            {user._id === userId ? (
+              <div>
+                <Button
+                  className={classes.button}
+                  type="button"
+                  variant="contained"
+                  size="large"
+                  color="primary"
+                  onClick={handleOpenAccount}
+                >
+                  Update Profile{" "}
+                </Button>
+                <Modal
+                  aria-labelledby="transition-modal-title"
+                  aria-describedby="transition-modal-description"
+                  className={classes.modal}
+                  open={openAccount}
+                  onClose={handleClose}
+                  closeAfterTransition
+                  BackdropComponent={Backdrop}
+                  BackdropProps={{
+                    timeout: 500
+                  }}
+                >
+                  <Fade in={openAccount}>
+                    <div className={classes.paper}>
+                      <Account />
+                    </div>
+                  </Fade>
+                </Modal>
+              </div>
+            ) : (
+              <div>
+                <Button
+                  className={classes.button}
+                  type="button"
+                  variant="contained"
+                  size="large"
+                  color="primary"
+                  onClick={handleOpenAccount}
+                >
+                  {user.profile.userType === "artist" ? "Message Artist" : "Message Venue"}
+                </Button>
+                <Modal
+                  aria-labelledby="transition-modal-title"
+                  aria-describedby="transition-modal-description"
+                  className={classes.modal}
+                  open={openAccount}
+                  onClose={handleClose}
+                  closeAfterTransition
+                  BackdropComponent={Backdrop}
+                  BackdropProps={{
+                    timeout: 500
+                  }}
+                >
+                  <Fade in={openAccount}>
+                    <div className={classes.paper}></div>
+                  </Fade>
+                </Modal>
+              </div>
+            )}
+            {user.profile.userType === "venue" && user._id === userId ? (
+              <div>
+                <Button
+                  className={classes.button}
+                  type="button"
+                  variant="contained"
+                  size="large"
+                  color="primary"
+                  onClick={handleOpenEvent}
+                >
+                  Create New Event
+                </Button>
+                <Modal
+                  aria-labelledby="transition-modal-title"
+                  aria-describedby="transition-modal-description"
+                  className={classes.modal}
+                  open={openEvent}
+                  onClose={handleClose}
+                  closeAfterTransition
+                  BackdropComponent={Backdrop}
+                  BackdropProps={{
+                    timeout: 500
+                  }}
+                >
+                  <Fade in={openEvent}>
+                    <div className={classes.paper}>
+                      <SubmitEvent />
+                    </div>
+                  </Fade>
+                </Modal>
+              </div>
+            ) : null}
+          </Box>
         </Box>
-
-        <Typography className={classes.description} variant="body1">
-          {user && user.profile && user.profile.description}
-        </Typography>
       </Card>
       {user && user.profile && user.profile.social ? (
-        <div>
+        <Box className={classes.social}>
           <Typography variant="h5">Connect with {user.profile.title} on Social Media</Typography>
-
-          <Link
-            to={user && user.profile && user.profile.social && user.profile.social.facebook}
-            target="_blank"
-          >
-            <FacebookIcon /> Facebook
-          </Link>
-          <Link
-            to={user && user.profile && user.profile.social && user.profile.social.instagram}
-            target="_blank"
-          >
-            <InstagramIcon /> Instagram
-          </Link>
-          <Link href={user.profile.social.twitter} onClick={preventDefault} target="_blank">
-            <TwitterIcon /> Twitter
-          </Link>
-        </div>
+          <Box className={classes.socialLinks}>
+            <Link
+              className={classes.link}
+              to={user && user.profile && user.profile.social && user.profile.social.facebook}
+              target="_blank"
+            >
+              <FacebookIcon className={classes.icon} /> Facebook
+            </Link>
+            <Link
+              className={classes.link}
+              to={user && user.profile && user.profile.social && user.profile.social.instagram}
+              target="_blank"
+            >
+              <InstagramIcon className={classes.icon} /> Instagram
+            </Link>
+            <Link
+              className={classes.link}
+              to={user.profile.social.twitter}
+              onClick={preventDefault}
+              target="_blank"
+            >
+              <TwitterIcon className={classes.icon} /> Twitter
+            </Link>
+          </Box>
+        </Box>
       ) : null}
-      {user.profile.userType === "venue" && user._id === userId ? (
-        <div>
-          <Button
-            type="button"
-            variant="contained"
-            size="large"
-            color="primary"
-            onClick={handleOpenEvent}
-          >
-            Create a New Event
-          </Button>
-          <Modal
-            aria-labelledby="transition-modal-title"
-            aria-describedby="transition-modal-description"
-            className={classes.modal}
-            open={openEvent}
-            onClose={handleClose}
-            closeAfterTransition
-            BackdropComponent={Backdrop}
-            BackdropProps={{
-              timeout: 500
-            }}
-          >
-            <Fade in={openEvent}>
-              <div className={classes.paper}>
-                <SubmitEvent />
-              </div>
-            </Fade>
-          </Modal>
-        </div>
-      ) : null}
+      {/* {user.profile.userType === "venue" && } */}
+      {console.log(pastEvents)}
     </Grid>
   ) : event ? (
     (console.log(event),
     (
-      <Grid>
+      <Grid className={classes.profileContainer}>
         <img src={event && event.imageurl} className={classes.banner} />
-        <Card>
-          {user ? <Gravatar className={classes.gravatar} email={user.emails[0].address} /> : null}
-          <Typography variant="h4"> {event.title}</Typography>
-          <Typography variant="h5" color="primary">
-            {event.location}
-          </Typography>
-          <Typography variant="subtitle1">{event.date}</Typography>
-          <Typography variant="body1">{event.description}</Typography>
-
-          {/* {event._id !== eventId ? <Button>Apply to Play</Button> : null} */}
+        <Card className={classes.eventCard}>
+          <Box>
+            <Typography variant="h4"> {event.title}</Typography>
+            <Typography variant="h5" color="primary">
+              {event.location}
+            </Typography>
+            <Typography variant="subtitle1">{event.date}</Typography>
+            <Typography variant="body1">{event.description}</Typography>
+          </Box>
+          {!event.filled ? (
+            <Button
+              className={classes.button}
+              type="button"
+              variant="contained"
+              size="large"
+              color="primary"
+            >
+              Apply to Event
+            </Button>
+          ) : (
+            <Typography variant="h5" color="primary">
+              Lineup Filled
+            </Typography>
+          )}
         </Card>
       </Grid>
     ))
