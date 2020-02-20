f9a46d657efdd80a3e7009c5d19f42f20a6805a0
Sina <sina202@gmail.com>
2020-02-19 16:17:11 -0800

    change theme properties

--- imports/ui/theme/index.js
+++ imports/ui/theme/index.js
@@ -3,13 +3,6 @@ import { createMuiTheme } from "@material-ui/core/styles";
 export default createMuiTheme({
   palette: {
     primary: {
-<<<<<<< HEAD
-      main: "#FF9633",
-      background: "#F7F7F7"
-    },
-    secondary: {
-      main: "#67687A"
-=======
       main: "#FF9633", //orange
       background: "#F7F7F7", //off-white
       title: "#000000", //black
@@ -17,17 +10,12 @@ export default createMuiTheme({
     },
     secondary: {
       main: "#67687A" //off-gray
->>>>>>> master
     }
   },
   fonts: {
     primary: {
-<<<<<<< HEAD
       main: "Roboto, sans-serif",
       color: "#303030"
-=======
-      main: "Roboto, sans-serif"
->>>>>>> master
     },
     secondary: {
       main: "Dhurjati, sans-serif"
