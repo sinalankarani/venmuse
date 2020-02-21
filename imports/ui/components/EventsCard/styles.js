00ee82069c4a210b2ff8cb23cb26786417deb19f
Brooke <brooke.xiang@gmail.com>
2020-02-21 12:29:43 -0800

    change styling for eventcard

--- imports/ui/components/EventsCard/styles.js
+++ imports/ui/components/EventsCard/styles.js
@@ -1,41 +1,56 @@
 const styles = theme => {
   return {
     card: {
-      position: 'relative',
+      position: "relative",
       height: 350
     },
     image: {
-      width: '100%',
+      width: "100%",
       height: 150,
       borderRadius: 5,
-      objectPosition: 'center',
-      objectFit: 'cover'
+      objectPosition: "center",
+      objectFit: "cover"
     },
     eventDetails: {
-      display: 'flex',
-      flexDirection: 'column',
+      display: "flex",
+      flexDirection: "column",
       padding: 10
     },
     link: {
-      textDecoration: 'none',
+      textDecoration: "none",
       color: theme.fonts.primary.color
     },
     notifyCounter: {
       height: 30,
       width: 30,
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius: '50%',
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      borderRadius: "50%",
       backgroundColor: theme.palette.primary.main,
-      zIndex: '1',
+      zIndex: "1",
       fontSize: 25,
-      alignSelf: 'flex-end',
-      boxShadow: '2px 1px 6px '
+      alignSelf: "flex-end",
+      boxShadow: "2px 1px 6px "
     },
     counter: {
-      color: 'white',
-      fontWeight: 'bold'
+      color: "white",
+      fontWeight: "bold"
+    },
+    date: {
+      fontFamily: theme.fonts.secondary.main,
+      textTransform: "uppercase"
+    },
+    lineup: {
+      backgroundColor: "rgba(255, 150, 51, 0.85)",
+      color: theme.palette.primary.background,
+      fontWeight: "bold",
+      border: "2px solid #fff",
+      position: "absolute",
+      top: "15%",
+      left: "50%",
+      transform: "translate(-50%)",
+      padding: "10px"
     }
   };
 };
